03858244a6d5c8bf0dbf10baacb0fd518ffb8e09
holma91 <alexanderholmberg91@gmail.com>
2023-06-14 13:54:36 +0200

    add webview stuff

--- services/mobile_app/screens/Browse.screen.tsx
+++ services/mobile_app/screens/Browse.screen.tsx
@@ -1,22 +1,37 @@
 import { FlatList, Image, SafeAreaView } from 'react-native';
-import {
-  createBox,
-  createText,
-  createRestyleComponent,
-  createVariant,
-  VariantProps,
-} from '@shopify/restyle';
+import { WebView } from 'react-native-webview';
+import { useState } from 'react';
 import { Theme } from '../styling/theme';
 import { Box } from '../styling/Box';
 import { Text } from '../styling/Text';
-import { useState } from 'react';
 
 export default function Browse() {
+  const runFirst = `
+      document.body.style.backgroundColor = 'red';
+      setTimeout(function() { window.alert('hi') }, 2000);
+      true; // note: this is required, or you'll sometimes get silent failures
+    `;
+
   return (
     <Box backgroundColor="background" flex={1}>
       <SafeAreaView style={{ flex: 1 }}>
         <Box flex={1} paddingHorizontal="m" gap="s">
-          <Text variant="title">url bar</Text>
+          <Text variant="header">Hey</Text>
+          <Box
+            flex={1}
+            margin="m"
+            borderWidth={2}
+            borderRadius={10}
+            borderColor="primary"
+          >
+            <WebView
+              source={{
+                uri: 'https://github.com/react-native-webview/react-native-webview',
+              }}
+              borderRadius={10}
+              injectedJavaScript={runFirst}
+            />
+          </Box>
         </Box>
       </SafeAreaView>
     </Box>
